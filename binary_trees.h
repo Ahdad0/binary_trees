2a4c5af10c1ac9999f272706c4ca72290f39faed
Ahdad0 <abderrahimahdadabdo@gmail.com>
2024-01-04 16:37:19 +0100

    updated

--- binary_trees.h
+++ binary_trees.h
@@ -51,5 +51,6 @@ size_t max(int a, int b);
 size_t find_tree_height(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif /*_BINARY_TREES_H_*/
